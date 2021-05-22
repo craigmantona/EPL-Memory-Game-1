a5209891f0bec24357bb0d9dd7dbafe5080bfb6f
craigmantona <craigmantona@hotmail.com>
2021-05-22 19:53:23 +0000

    Shuffle card function added to game.js file. Still some issues with cards unflipping to be dealt with

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,10 +2,14 @@
 const cards = document.querySelectorAll('.team-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-  this.classList.toggle('flip');
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+   this.classList.add('flip');
 }
 
 if (!hasFlippedCard) {
@@ -14,14 +18,13 @@ if (!hasFlippedCard) {
   firstCard = this;
 } else {
   //second click
-  hasFlippedCard = false;
   secondCard = this;
 
   checkForMatch();
 }
 
 function checkForMatch() {
-  let isMatch = firstCard.dataset.team === secondCard.dataset.team;
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
   isMatch ? disableCards() : unflipCards();
 }
@@ -30,15 +33,32 @@ function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
 
+  resetBoard();
+
 }
 
 function unflipCards() {
+  lockBoard = true;
   //not match
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
+
+    resetBoard();
   }, 1500);
 
 }
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
